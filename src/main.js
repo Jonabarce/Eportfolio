953e13a76d8e24649e5e20d8f1cd20d82745d465
Jonatan Andre Vevang <jonatanv@stud.ntnu.no>
2023-02-12 21:43:41 +0100

    Added vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/routers';
+import { inject } from '@vercel/analytics';
 
 createApp(App).use(router).mount('#app')
+
+inject();
